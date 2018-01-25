3dc8529b828ad297c23818f8ba0729fca014a7eb
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-25 01:07:22 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,28 @@
 def consolidate_cart(cart)
   # code here
+  consolidated = {}
+
+  cart.each do |hash|
+    hash.each do |item, information|
+      if !consolidated.keys.include?(item)
+        consolidated[item] = information
+        consolidated[item][:count] = 1
+      else
+        consolidated[item][:count] += 1
+      end
+    end
+  end
+
+  consolidated
+
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
+  cart.each do |item, info|
+    coupons.each do |coupon, specs|
+    end
+  end
 end
 
 def apply_clearance(cart)
